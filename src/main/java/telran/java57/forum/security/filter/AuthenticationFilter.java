1b9eaed8b7d9f65d2c80bc411947b8fd3fabed3d
Alexander <algurev0220@gmail.com>
2025-04-11 10:27:01 +0300

    checkEndPoint

---
+++ src/main/java/telran/java57/forum/security/filter/AuthenticationFilter.java
@@ -0,0 +1,74 @@
+package telran.java57.forum.security.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequestWrapper;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+import telran.java57.forum.accounting.dao.UserAccountRepository;
+import telran.java57.forum.accounting.dto.exceptions.UserNotFoundException;
+import telran.java57.forum.accounting.dto.exceptions.WrongPasswordException;
+import telran.java57.forum.accounting.model.UserAccount;
+
+import java.io.IOException;
+import java.security.Principal;
+import java.util.Base64;
+import java.util.Optional;
+
+@Component
+@Order(10)
+@RequiredArgsConstructor
+public class AuthenticationFilter implements Filter {
+    final UserAccountRepository userAccountRepository;
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        if (checkEndPoint(request.getMethod(), request.getServletPath())) {
+            String authorizationHeader = request.getHeader("Authorization");
+            if (authorizationHeader != null) {
+                String[] credentials = getCredentials(authorizationHeader);
+                Optional<UserAccount> userAccount = userAccountRepository.findById(credentials[0]);
+                String login = userAccount.isPresent() ?
+                        BCrypt.checkpw(credentials[1], userAccount.get().getPassword()) ?
+                                credentials[0] :
+                                WrongPasswordException.exceptionName :
+                        UserNotFoundException.exceptionName;
+                request = new WrappedRequest(request, login);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private boolean checkEndPoint(String method, String path) {
+        return HttpMethod.POST.matches(method) && path.matches("/login");
+    }
+
+    private String[] getCredentials(String headerValue) {
+        String token = headerValue.split(" ")[1];
+        String decode = new String(Base64.getDecoder().decode(token));
+        return decode.split(":");
+    }
+
+    private class WrappedRequest extends HttpServletRequestWrapper {
+        private String login;
+
+        public WrappedRequest(HttpServletRequest request, String login) {
+            super(request);
+            this.login = login;
+        }
+
+        @Override
+        public Principal getUserPrincipal() {
+            return () -> {
+                return login;
+            };
+        }
+    }
+}
